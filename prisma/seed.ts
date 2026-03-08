a8a0b535e2bef667eae331b39160e0a726129955
Bagas Diatama Wardoyo <diatama.w8@gmail.com>
2026-03-08 15:46:38 +0700

    add database migration and seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,8 @@
-import { PrismaClient } from ;
+import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaPg} from "@prisma/adapter-pg"
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env["DATABASE_URL"]! });
+const prisma = new PrismaClient({adapter});
 
 async function main() {
     console.log("seeding");
@@ -19,6 +21,17 @@ async function main() {
         { name: 'Cintamulya' },
     ]
 
-    await prisma
+    await prisma.village.createMany({
+        data: villages,
+        skipDuplicates:true
+    })
 
+    console.log("sedding berhasil");
 }
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1)
+}).finally(async () => {
+    await prisma.$disconnect();
+})
