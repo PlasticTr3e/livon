a9e6a5f071f09c957e8ef3c66e426c0e705eabaa
Bagas Diatama Wardoyo <diatama.w8@gmail.com>
2026-03-19 22:26:00 +0700

    CS test

---
+++ src/services/sentiment.ts
@@ -0,0 +1,44 @@
+import { SentimentLabel } from "@/generated/prisma/enums";
+
+export function analyzeSentiment(text: string): {
+  score: number;
+  label: SentimentLabel;
+} {
+  const positiveWords = [
+    "bagus",
+    "mantap",
+    "keren",
+    "setuju",
+    "mendukung",
+    "cepat",
+    "solusi",
+    "terima kasih",
+  ];
+  const negativeWords = [
+    "rusak",
+    "hancur",
+    "lambat",
+    "jelek",
+    "kecewa",
+    "parah",
+    "bahaya",
+    "macet",
+    "bolong",
+  ];
+
+  let score = 0;
+
+  const words = text.toLocaleLowerCase().split(/\W+/);
+
+  words.forEach((word) => {
+    if (positiveWords.includes(word)) score += 1;
+    if (negativeWords.includes(word)) score -= 1;
+  });
+
+  let label: SentimentLabel = SentimentLabel.NEUTRAL;
+
+  if (score > 0) label = SentimentLabel.POSITIVE;
+  if (score < 0) label = SentimentLabel.NEGATIVE;
+
+  return { score, label };
+}
