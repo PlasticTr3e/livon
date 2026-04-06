43ebb722c6f3ff70f0985509fbf2c4bb0f71b533
jpr-exe <144073608+jpr-exe@users.noreply.github.com>
2026-04-06 21:45:47 +0700

    feat: add profile management and update comments API

---
+++ src/app/api/profile/me/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET(req: Request) {
+  try {
+    const userId = req.headers.get('user-id'); 
+
+    if (!userId) return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        citizenProfile: true, 
+        agencyProfile: true, 
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    return NextResponse.json({ message: "Error" }, { status: 500 });
+  }
+}
---
+++ src/app/api/profile/update/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function PATCH(req: Request) {
+  try {
+    const userId = req.headers.get('user-id'); 
+    const { fullname, address } = await req.json();
+    if (!userId) {
+      return NextResponse.json({ message: "User ID wajib diisi" }, { status: 400 });
+    }
+
+    const updatedProfile = await prisma.citizenProfile.update({
+      where: {
+        userId: userId, 
+      },
+      data: {
+        fullName: fullname, 
+        address: address, 
+      },
+    });
+
+    return NextResponse.json({
+      message: "Profil berhasil diperbarui",
+      data: updatedProfile,
+    });
+
+  } catch (error: any) {
+    console.error("Update Profile Error:", error);
+    
+    if (error.code === 'P2025') {
+      return NextResponse.json({ message: "Profil tidak ditemukan" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Gagal memperbarui profil" }, { status: 500 });
+  }
+}
